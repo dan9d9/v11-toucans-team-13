937a38362ab49a80eb383ef5e48c17073717b7fe
Daniel Dick <dan9d9@gmail.com>
2019-09-10 17:36:25 +0200

    remove showHamburger function

--- main.js
+++
@@ -1,9 +0,0 @@
-function showHamburger() {
-    let hamburger = document.querySelector('.hamburger');
-
-    if(window.innerWidth <= 998) {
-        hamburger.classList.add('hamburger_visible');
-    } else {hamburger.classList.remove('hamburger_visible');}
-}
-
-window.addEventListener('resize', showHamburger);
