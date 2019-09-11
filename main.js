bbf83b3406e329131e4257e8c66149877715308d
Daniel Dick <dan9d9@gmail.com>
2019-09-11 12:34:55 +0200

    add resize listener to auto close mobileNav

--- main.js
+++ main.js
@@ -11,11 +11,16 @@ function openNav() {
     const btnClose = document.querySelector('.hamburger_close');
     
     mobileNav.style.left = 0;
-    body.style.overflow = 'hidden';
+    body.style.overflow = 'hidden'; // Prevent scrolling when mobileNav is active
 
     btnClose.addEventListener('click', closeNav);
+    window.addEventListener('resize', function() {  // Close mobile section automatically if
+        if(window.innerWidth >= 998) {              // window is resized above 998px
+            closeNav();
+        }
+    })
 
-    mobileNav.addEventListener('touchmove', function(e) {
+    mobileNav.addEventListener('touchmove', function(e) {   // Prevent scroll on touch devices (Needs tested)
         e.preventDefault();
     })
 }   
