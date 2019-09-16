8087d20a168ce6adf63fc4d60640d75872b5bc8b
Vanesa <vanesa.perea.cuevas@gmail.com>
2019-09-16 15:30:41 +0200

    "Add background image style"

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 
+/***Background style***/
+var background = document.querySelector('#dropDownSection');
+background.style.backgroundImage = "url('bg-text.png')";
+background.style.size = "cover";
+background.style.height = "200px";
+
