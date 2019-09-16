b94e070442d2d7d4b3bc7b49b608ea7266169533
Vanesa <vanesa.perea.cuevas@gmail.com>
2019-09-16 15:44:14 +0200

    "Change url dropDownMenu background"

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 //Background style
 var background = document.querySelector('#dropDownSection');
-background.style.backgroundImage = "url('bg-text.png')";
+background.style.backgroundImage = "url('/Chipotle Clone Images/bg-text.png')";
 background.style.size = "cover";
 background.style.height = "200px";
 
