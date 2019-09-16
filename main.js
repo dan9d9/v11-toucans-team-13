86ec1033630820466f9d04cc96481a80151e075e
Vanesa <vanesa.perea.cuevas@gmail.com>
2019-09-16 15:34:26 +0200

    "Add width to div"

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 
-/***Background style***/
+//Background style
 var background = document.querySelector('#dropDownSection');
 background.style.backgroundImage = "url('bg-text.png')";
 background.style.size = "cover";
 background.style.height = "200px";
+
+//Div width
+var backgroundText = document.querySelector('.bgText');
+backgroundText.style.width = "100%";
+
 
