6be8aec38dfef84ccd829065c8bd53b3a09ba596
Daniel Dick <dan9d9@gmail.com>
2020-05-05 12:30:56 +0200

    remove styles from js

--- main.js
+++ main.js
@@ -43,29 +43,6 @@ function repositionBackground() {
  
 window.addEventListener('resize', repositionBackground);
 
-// DROPDOWN MENU //
-
-//Background style
-var background = document.querySelector('#dropDownSection');
-background.style.backgroundImage = "url('./Chipotle-Clone-Images/bg-text-min.png')";
-background.style.size = "cover";
-background.style.height = "200px";
-
-//Div width
-var backgroundText = document.querySelector('.bgText');
-backgroundText.style.width = "100%";
-
-//Text p style
-var text = document.querySelector('.dropText');
-text.style.backgroundColor = "#F7F2ED";
-text.style.padding = "1%";
-text.style.color = "#451400";
-text.style.fontWeight = "#bold";
-
-//Border bottom line
-var select = document.querySelector('select');
-select.style.borderBottom = "3px solid #451400";
-
 //Animation select names (rotation)
 
 function textRotator(n){
